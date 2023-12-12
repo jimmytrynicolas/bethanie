aed9d58813239b7632f41deb4ccc69846c395ca1
jimmytrynicolas <153224820+jimmytrynicolas@users.noreply.github.com>
2023-12-12 12:53:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function changerimage()
+{
+    document.getElementById("changer").src="images/ns4.jpg";
+}
+
+function affichermenu()
+{
+  if (document.getElementById("menu").style.display =="block")
+  {
+    document.getElementById("menu").style.display ="none"
+  }
+  else
+  {
+    document.getElementById("menu").style.display ="block";
+  }
+}
